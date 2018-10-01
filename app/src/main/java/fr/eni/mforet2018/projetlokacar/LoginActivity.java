851e136c9e2265ae0ca526415416d8ecfee22e37
forestator <forestator@gmail.com>
2018-10-01 10:29:19 +0200

    Bonjour premier commit c'est super du super projet super de super location de voiture super -> projet super android

---
+++ app/src/main/java/fr/eni/mforet2018/projetlokacar/LoginActivity.java
@@ -0,0 +1,13 @@
+package fr.eni.mforet2018.projetlokacar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
