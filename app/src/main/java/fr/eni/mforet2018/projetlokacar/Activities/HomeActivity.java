d1e725bfab52f84986343546496fe1f4708c885f
forestator <forestator@gmail.com>
2018-10-02 09:18:06 +0200

    Ajout de RentCarActivity

--- app/src/main/java/fr/eni/mforet2018/projetlokacar/Activities/HomeActivity.java
+++ app/src/main/java/fr/eni/mforet2018/projetlokacar/Activities/HomeActivity.java
@@ -11,6 +11,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //TODO: Demande d'autorisations pour accès camera ...
+        //TODO: Demande d'autorisations pour accès camera ......
     }
 }
